42b25263b3b61e4efbd07145f61193d8d3a182e3
Jordan Ell <jordan.ell7@gmail.com>
2015-09-15 20:21:42 -0700

    Mail should work in production now.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,14 +80,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Email configuration
-  config.action_mailer.default_url_options = { :host => 'www.sewuniquebybonnie.com' }
+  config.action_mailer.default_url_options = { :host => 'sewuniquebybonnie.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            "#{ENV['GMAIL_USERNAME']}",
-    password:             "#{ENV['GMAIL_PASSWORD']}",
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       :login,
     enable_starttls_auto: true
   }
